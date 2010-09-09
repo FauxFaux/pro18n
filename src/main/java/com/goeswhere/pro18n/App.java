6b3f81219a420426c1a43cdd4f900af68a06685d
Chris West (Faux) <git@goeswhere.com>
2010-09-09 17:12:38 +0100

    mvn archetype:create sample

---
+++ src/main/java/com/goeswhere/pro18n/App.java
@@ -0,0 +1,13 @@
+package com.goeswhere.pro18n;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
